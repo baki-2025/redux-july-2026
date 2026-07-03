66ce4f04894f16e8951253178e18c52ac63f55ab
Abdullah Baki <abz.147570@gmail.com>
2026-07-03 22:19:58 +0600

    store

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const createStore = require('redux');
 //defining constants
 const INCREMENT = 'INCREMENT';
 const DECREMENT = 'DECREMENT';
@@ -54,4 +55,10 @@ const counterReducer = (state=initialCounterState, action) => {
 //1. state
 //2. dispatch action
 //3. reducer
-//4. store
+//4. store - getState(),dispatch(),subscribe()
+
+//create store
+const store = createStore(counterReducer);
+store.subscribe(() => {
+    console.log(store.getState());
+});
