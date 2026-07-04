168d5a537286ee4f399ff4cc2a181d455d7f27fc
Abdullah Baki <abz.147570@gmail.com>
2026-07-04 08:36:08 +0600

    incre/decre

---
+++ test.js
@@ -0,0 +1,34 @@
+//state -count:0
+//action -increment, decrement, reset
+//reducer 
+//store
+
+//CONSTANTS
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const RESET = 'RESET';
+
+const counterInitialState = {
+    count: 0
+};
+
+const incrementCounterAction = () => {
+    return {
+        type: INCREMENT,
+        
+    };
+}
+const decrementCounterAction = () => {
+    return {
+        type: DECREMENT,
+        
+    };
+}
+const resetCounterAction = () => {
+    return {
+        type: RESET,
+        
+    };
+}
+
+
